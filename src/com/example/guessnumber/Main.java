808cccf5f6633f6f10c76213efd3f7d24d8c7c7b
gontoblez <gontoblez@gmail.com>
2024-01-30 00:41:19 +0300

    game now has 3 levels. yippieee

--- src/com/example/guessnumber/Main.java
+++ src/com/example/guessnumber/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome to the game!");
         System.out.print("Please enter your name: ");
+        Scanner scanner = new Scanner(System.in);
         Scanner nameScanner = new Scanner(System.in);
         String name = nameScanner.next();
         System.out.println("Hello, " + name + "!");
@@ -15,66 +16,101 @@ public class Main {
         Scanner consentScanner = new Scanner(System.in);
         String consent = consentScanner.next();
 
+
         // let the user play if they want to play :P
         if (Objects.equals(consent, "y")) {
             System.out.println(" ");
             System.out.println("Alright. Let's begin!");
-
-            // generate random number
-            Random randomNum = new Random();
-            int number = randomNum.nextInt(50) + 1;
+            int level = 1;
+            boolean play = true;
 
             // instructions
-            System.out.println("A random number between 1 and 50 has been generated.");
-            System.out.println("You have 5 tries to guess the number.");
-            System.out.println("With every wrong guess, the game will give you a hint to raise or lower your guess");
-            System.out.println("Good luck!");
-
-            // ask the user
-            for (int i=0; i<5; i++) {
-                System.out.println(" ");
-                System.out.print("Enter your guess: ");
-                Scanner answer = new Scanner(System.in);
-                int guess = answer.nextInt();
-
-                if (guess > 50 || guess < 1) {
-                    System.out.println("The number is between 1 and 50!");
-//                    System.out.println("You still have 5 chances. You're outstandingly stupid.");
-//                    System.out.println("Therefore, none of your chances has been utilized, silly ;).");
-                    System.out.println("You still have 5 chances.");
-                    System.out.println("Try again...");
-                    i--;
-                    continue;
+            while (level < 4 && play) {
+                System.out.println(level);
+                if (level == 3) {
+                    play = false;
+                }
+                // generate random number
+                Random randomNum = new Random();
+                int number = 0;
+                if (level == 1) {
+                    number =randomNum.nextInt(50) + 1;
+                } else if (level == 2) {
+                    number = randomNum.nextInt(70) + 1;
+                } else if (level == 3) {
+                    number = randomNum.nextInt(100) + 1;
                 }
 
-                int triesLeft = 4-i;
+                // ask the user
+                System.out.println("Welcome to level " + level + "!");
+                System.out.print("A random number between 1 and ");
+                if (level == 1) {
+                    System.out.print("50");
+                } else if (level == 2) {
+                    System.out.print("70");
+                } else if (level == 3) {
+                    System.out.print("100");
+                }
+                System.out.print(" has been generated.\n");
+                System.out.println("You have 5 tries to guess the number.");
+                System.out.println("With every wrong guess, the game will give you a hint to raise or lower your guess");
+                System.out.println("Good luck!");
 
-                // processing input
-                if (guess > number) {
-                    System.out.println("Wrong!");
-                    if (triesLeft != 0) {
-                        System.out.println("Guess lower!");
-                    }
-                    System.out.println("Note: You got " + triesLeft + " more chance(s).");
-                    if (triesLeft == 0) {
-                        System.out.println("You lost the game.");
-                        System.out.println("The correct answer was: " + number);
+                for (int i = 0; i < 5; i++) {
+                    System.out.println(number);
+                    System.out.println(" ");
+                    System.out.print("Enter your guess: ");
+                    Scanner answer = new Scanner(System.in);
+                    int guess = answer.nextInt();
+
+                    int triesLeft = 4 - i;
+
+                    // processing input
+                    if (guess > number) {
+                        System.out.println("Wrong!");
+                        if (triesLeft != 0) {
+                            System.out.println("Guess lower!");
+                        }
+                        System.out.println("Note: You got " + triesLeft + " more chance(s).");
+                        if (triesLeft == 0) {
+                            System.out.println("You lost the game.");
+                            System.out.println("The correct answer was: " + number);
 //                        System.out.println("You're a mistake. Your entire life is a joke!");
-                    }
-                } else if (guess < number) {
-                    System.out.println("Wrong!");
-                    if (triesLeft != 0) {
-                        System.out.println("Guess higher!");
-                    }
-                    System.out.println("Note: You got " + triesLeft + " more chance(s).");
-                    if (triesLeft == 0) {
-                        System.out.println("You lost the game.");
-                        System.out.println("The correct answer was: " + number);
+                        }
+                    } else if (guess < number) {
+                        System.out.println("Wrong!");
+                        if (triesLeft != 0) {
+                            System.out.println("Guess higher!");
+                        }
+                        System.out.println("Note: You got " + triesLeft + " more chance(s).");
+                        if (triesLeft == 0) {
+                            System.out.println("You lost the game.");
+                            System.out.println("The correct answer was: " + number);
+                            play = false;
+                            break;
 //                        System.out.println("You're a mistake. Your entire life is a joke!");
+                        }
+                    } else {
+                        System.out.println("Congratulations, you have guessed the number!");
+                        triesLeft = 0;
+                        System.out.println("You now have " + triesLeft + " tries left. The number of tries will now be reset.");
+                        if (level < 3) {
+                            System.out.print("Would you like to progress to the next level ?[Y/n]\t");
+                            String continu = scanner.next();
+                            if (continu.equals("N") || continu.equals("n")) {
+                                break;
+                            } else if (continu.equals("Y") || continu.equals("y") || continu.equals("")) {
+                                level++;
+                                System.out.println(level);
+                                triesLeft = 4;
+                                break;
+                            }
+                        } else if (level == 3) {
+                            System.out.println("You have beaten the game. Congratulations!");
+                            play = false;
+                            break;
+                        }
                     }
-                } else {
-                    System.out.println("Congratulations, you have guessed the number!");
-                    break;
                 }
             }
 
